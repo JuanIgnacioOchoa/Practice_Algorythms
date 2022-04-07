070b26e99809bddaa8be620d6757f345219517e0
Juan Ochoa <juan.ochoa@adira.com.mx>
2022-04-07 09:34:30 -0700

    Chain Reactions final test solved

--- Google/CodeJam2022/Qualifications/Chain Reactions/main.py
+++ Google/CodeJam2022/Qualifications/Chain Reactions/main.py
@@ -1,25 +1,76 @@
 
-from re import I
-from sys import stdin, stdout
 from collections import defaultdict
 from copy import deepcopy
+from sys import stdin, stdout
 import os
 import time
 import itertools
-from unittest import result
+
 
 start_time = time.time()
 
+class Node():
+    def __init__(self, n, w):
+        self.n = n
+        self.w = w
+        self.children = []
+        self.father = None
+
+    def addChild(self, child):
+        child.father = self.n
+        self.children.append(child)
+
+    def dfs(self):
+        global score
+        if len(self.children) == 0:
+            return self.w
+        childAnswer = []
+        for n in self.children:
+            childAnswer.append(n.dfs())
+        childAnswer.sort()
+        for i in range(1, len(self.children)):
+            score+=childAnswer[i]
+        
+        return max(self.w, childAnswer[0])
+
+def getScore(nodes, P):
+    ans = 0
+    ini = getInitiators(P)
+    res = []
+    N = len(P)
+    
+    while N > 0:
+        while N not in ini and N > 0:
+            N -= 1
+        curr = nodes[N]
+        
+        childAnswers = []
+        for n in curr.children:
+            childAnswers.append(n.w)
+        childAnswers.sort()
+        for i in range(1, len(childAnswers)):
+            res.append(childAnswers[i])
+            ans += childAnswers[i]
+        if len(childAnswers):
+            curr.w = max(curr.w, childAnswers[0])
+        if curr.father == 0:
+            ans += curr.w
+            N-=1
+            continue
+        ini[curr.father] = False
+        N-=1
+        #ini.sort()
+    return ans
 
 def getInitiators(P):
-    indicators = []
+    indicators = {}
     modules = set()
     for i in range(len(P)):
         modules.add(P[i])
 
     for i in range(len(P)):
         if (i + 1) not in modules:
-            indicators.append(i)
+            indicators[i + 1] = False
     return indicators
 
 ## TEST 1
@@ -53,27 +104,14 @@ def getFun2(N, F, P):
     return ans
 
 
-# TEST 2
-def getScore(N, F, P):
-    ini = getInitiators(P)
-    score = 0
-    for i in ini:
-        curr = i
-        prev = curr
-        while curr != -1:
-            
-            father = P[curr] - 1
-            if father == -1:
-                score += F[curr]
-                F[curr] = F[prev]
-            else:
-                if F[curr] > F[father]:
-                    F[father], F[curr] = F[curr], F[father]
-            prev = curr
-            curr = father
-    return score
+def buildTree(nodes, F, P):
+    nodes[0] = Node(0, -1)
+    n = len(F)
+    for i in range(n):
+        nodes[i + 1] = Node(i + 1, F[i])
+        nodes[P[i]].addChild(nodes[i + 1])
+    return
 dir = os.path.dirname(__file__)
-
 
 #stdin = open(os.path.join(dir,  'test_data/test_set_1/ts1_input.txt'),'r')
 #stdout = open(os.path.join(dir, 'test_data/test_set_1/ts1_output.txt'),'r')
@@ -83,10 +121,14 @@ stdout = open(os.path.join(dir, 'test_data/sample_test_set_1/sample_ts1_output.t
 t = int(stdin.readline())
 
 for case in range(t):
+    score = 0
     N = int(stdin.readline().strip())
     F = list(map(int, stdin.readline().split()))
     P = list(map(int, stdin.readline().split()))
-    score = getScore(N, F, P)
+    nodes = [None] * (N + 1)
+    buildTree(nodes, F, P)
+    score = getScore(nodes, P)
+    
     res = stdout.readline().strip()
     ans = ('Case #%d: %s') % (case+1, score)
     if ans != res:
